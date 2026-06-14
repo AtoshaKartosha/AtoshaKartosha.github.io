bf3b7d27cc234e1ad35a6c8db43e78dfb74206d4
dmitrygusarrr <dmitry.gusarrr@adsuitech.pro>
2026-06-14 07:20:26 +0300

    style: lower folder, move map right and up to overlap folder, move clock to clear map

--- frontend/src/data/boardItems.ts
+++ frontend/src/data/boardItems.ts
@@ -23,7 +23,7 @@ export const boardItems: BoardItem[] = [
     name: "Главное дело",
     popupId: "event-info",
     zIndex: 14,
-    desktop: { left: 34, top: 14, width: 26, rotation: -2 },
+    desktop: { left: 34, top: 18, width: 26, rotation: -2 },
     mobile: { left: 28, top: 32, width: 42, rotation: -2 },
   },
   {
@@ -55,7 +55,7 @@ export const boardItems: BoardItem[] = [
     name: "Карта Вокзал 1853",
     popupId: "location",
     zIndex: 15,
-    desktop: { left: 51, top: 58, width: 13, rotation: -4 },
+    desktop: { left: 53, top: 48, width: 13, rotation: -4 },
     mobile: { left: 28, top: 72, width: 32, rotation: -4 },
   },
   {
@@ -71,7 +71,7 @@ export const boardItems: BoardItem[] = [
     name: "Часы начала",
     popupId: "schedule",
     zIndex: 12,
-    desktop: { left: 64, top: 52, width: 8, rotation: -12 },
+    desktop: { left: 64, top: 38, width: 8, rotation: -12 },
     mobile: { left: 44, top: 12, width: 20, rotation: -12 },
   },
   {
