94ed1ce566ce8e8c9fd426e7d360d2b88ed3e944
dmitrygusarrr <dmitry.gusarrr@adsuitech.pro>
2026-06-19 10:41:33 +0300

    chore: remove unused webpack config to build successfully with turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ["127.0.0.1", "localhost", "26.222.75.8"],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      type: "asset/source",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
