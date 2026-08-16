b97951f2a1d372411473d8c88eda4cf26786a1cd
AtoshaKartosha <AtoshaKartosha@users.noreply.github.com>
2026-08-16 06:52:31 +0300

    fix(vercel): remove unavailable React compiler

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
   allowedDevOrigins: ["127.0.0.1", "localhost", "26.222.75.8"],
   output: "export",
   images: {
